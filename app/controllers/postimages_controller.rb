f31d77ba34d03ec758899edc4ecb2da69855eb59
825814 <120297399+825814@users.noreply.github.com>
2023-01-19 11:56:32 +0000

    [finish]4-9

---
+++ app/controllers/postimages_controller.rb
@@ -0,0 +1,10 @@
+class PostimagesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'postimages/new'
+  # get 'postimages/index'
+  # get 'postimages/show'
+  resources :post_images, only: [:new, :index, :show]
   devise_for :users
   root to: "homes#top"
   get 'homes/about', as: 'about'
