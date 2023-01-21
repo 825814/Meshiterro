052cc5b81f7aaf426470d60837a49cd792955cd2
825814 <120297399+825814@users.noreply.github.com>
2023-01-21 12:36:46 +0000

    [finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # get 'postimages/new'
   # get 'postimages/index'
   # get 'postimages/show'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   devise_for :users
   root to: "homes#top"
