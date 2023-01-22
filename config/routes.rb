11472516c4cf13bdd600fcd3921ffef7a61fcbb8
825814 <120297399+825814@users.noreply.github.com>
2023-01-22 01:19:31 +0000

    [finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # get 'postimages/index'
   # get 'postimages/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
